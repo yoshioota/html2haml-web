c9c6e502dfed77e4887b8c87b5841c86b788aa45
yoshi oota <yoshi.oota@gmail.com>
2014-03-10 21:01:28 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Html2hamlWeb::Application.config.secret_key_base = 'b6dce2c8a8f10e25d2dc594dc23cfd9670c3ab9bfa19491215117429064720a01f6c1318f0fc8a165891d6e6cf46b7853d3865be3decd933f84f24a9089430bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Html2hamlWeb::Application.config.session_store :cookie_store, key: '_html2haml-web_session'
